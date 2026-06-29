f76ddd404b5b43440431afd0e2e15652e56bd61e
Josua Lindemann <josua.lindemann@outlook.de>
2026-06-29 11:04:38 +0200

    origin exclusion from training config

--- examples/cartpole/bisect_certify.py
+++ examples/cartpole/bisect_certify.py
@@ -74,7 +74,6 @@ def _build_certification_defaults(
         cert_bounds=cert_bounds,
         bins_per_dim=2,
         center_refinement_factor=1.0,
-        origin_exclusion=0.0,
         cert_method="crown",
         condition_margin=float(training_config.condition_margin),
         suppress_native_output=True,
